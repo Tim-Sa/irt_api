ea2382777f344e359678914705228b2fe63cc119
Tim-Sa <salnikovtimofeyof@gmail.com>
2023-05-28 23:07:11 +0300

    simple irt model with one learning step

--- irt.py
+++ irt.py
@@ -3,8 +3,47 @@ import pandas as pd
 from pandas import DataFrame
 
 
+def prepare(df: DataFrame):
+    '''
+    Remove consisting only of zeros columns and rows.
+
+        Parameters:
+                df (DataFrame): A matrix with zeros and ones values only.
+
+        Returns:
+                df (DataFrame): Filtered matrix.
+                rejected_subjects (list): List of subjects, who only have zero results.
+                rejected_tasks (list): List of tasks, who got only zero results.
+    '''
+    subjects = df.index
+    tasks = df.columns
+    # remove subjects with only zeros.
+    check_subject = lambda subject: df.loc[subject].sum() == 0
+    rejected_subjects = list(filter(check_subject, subjects))
+    df = df.drop(index=rejected_subjects)
+    # remove tasks with only zeros.
+    check_task = lambda task: df[task].sum() == 0
+    rejected_tasks = list(filter(check_task, tasks))
+    df = df.loc[:, ~df.columns.isin(rejected_tasks)]
+
+    return df, rejected_subjects, rejected_tasks    
+
+
 def irt(df: DataFrame):
-    pass
+    '''
+    Calculation of scores for the subjects of their tasks 
+    in the form of logits on the IRT model.
+
+        Parameters:
+                df (DataFrame): A matrix with zeros and ones values only.
+        
+        Returns:
+                ability (1d np.array): Binary array of subjects ability.
+                difficult (1d np.array): Binary array of tasks difficult.
+    '''
+    df, _, _ = prepare(df)
+    matrix = df.to_numpy()
+    return iter_step(matrix)
 
 
 def iter_step(matrix: np.array):
@@ -12,12 +51,12 @@ def iter_step(matrix: np.array):
     Calculate subjects ability and task difficult in test as logits. 
     Accepts test results in binary form as input.
 
-            Parameters:
-                    matrix (2d np.array): A matrix with zeros and ones values only.
+        Parameters:
+                matrix (2d np.array): A matrix with zeros and ones values only.
 
-            Returns:
-                    ability (1d np.array): Binary array of subjects ability.
-                    difficult (1d np.array): Binary array of tasks difficult.
+        Returns:
+                ability (1d np.array): Binary array of subjects ability.
+                difficult (1d np.array): Binary array of tasks difficult.
     '''
     subjects_mean = np.mean(matrix, axis=1)
     ability = np.log(subjects_mean / (1 - subjects_mean))
@@ -26,4 +65,3 @@ def iter_step(matrix: np.array):
     difficult = np.log((1 - tasks_mean) / tasks_mean)
 
     return ability, difficult
-
--- test.py
+++ test.py
@@ -2,8 +2,9 @@ import unittest
 
 from itertools import combinations_with_replacement
 import numpy as np
+import pandas as pd
 
-from irt import iter_step
+from irt import irt, iter_step
 
 
 class TestIrt(unittest.TestCase):
@@ -19,6 +20,23 @@ class TestIrt(unittest.TestCase):
         for shape in shapes:
             iter_step(np.random.binomial(1, 0.5, size=shape))
 
+    def test_irt_pipeline(self):
+            df = pd.DataFrame({
+                'task1': [1, 1, 0, 0],
+                'task2': [0, 0, 1, 0],
+                'task3': [1, 0, 1, 0],
+                'task4': [0, 0, 0, 0]    
+            }, index=['subj1', 'subj2', 'subj3', 'subj4'])
+            true_abilities = np.array([0.69314718, 
+                                       -0.69314718,  
+                                       0.69314718])
+            true_difficult = np.array([-0.69314718, 
+                                       0.69314718,  
+                                       -0.69314718])
+            
+            abilities, difficult = irt(df)
+            self.assertTrue(np.allclose(abilities, true_abilities))
+            self.assertTrue(np.allclose(difficult, true_difficult))
 
 if __name__ == '__main__':
     unittest.main()
