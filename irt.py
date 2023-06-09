7a397e0f0857e05c94957f580790b6ba0f02f222
Tim-Sa <salnikovtimofeyof@gmail.com>
2023-06-09 15:48:38 +0300

    estimated values calc

--- irt.py
+++ irt.py
@@ -17,11 +17,11 @@ def prepare(df: DataFrame):
     '''
     subjects = df.index
     tasks = df.columns
-    # remove subjects with only zeros.
+    # Remove subjects with only zeros.
     check_subject = lambda subject: df.loc[subject].sum() == 0
     rejected_subjects = list(filter(check_subject, subjects))
     df = df.drop(index=rejected_subjects)
-    # remove tasks with only zeros.
+    # Remove tasks with only zeros.
     check_task = lambda task: df[task].sum() == 0
     rejected_tasks = list(filter(check_task, tasks))
     df = df.loc[:, ~df.columns.isin(rejected_tasks)]
@@ -41,12 +41,23 @@ def irt(df: DataFrame):
                 ability (1d np.array): Binary array of subjects ability.
                 difficult (1d np.array): Binary array of tasks difficult.
     '''
+    # Remove zeros Series from input data.
     df, _, _ = prepare(df)
     matrix = df.to_numpy()
-    return iter_step(matrix)
 
 
-def iter_step(matrix: np.array):
+    # Get prediction about test subjects ability and task diffiult.  
+    ability, difficult = predict(matrix)
+    # Shift vector of difficults to zero mean.
+    bias_difficult = difficult - difficult.mean()
+    # Try to minimize difference between estimated and real values
+    ev = estimated_values(ability, bias_difficult)
+    # TODO: Dispersion of estimated values.
+
+    return predict(ev)
+
+
+def predict(matrix: np.array):
     '''
     Calculate subjects ability and task difficult in test as logits. 
     Accepts test results in binary form as input.
@@ -65,3 +76,24 @@ def iter_step(matrix: np.array):
     difficult = np.log((1 - tasks_mean) / tasks_mean)
 
     return ability, difficult
+
+
+def estimated_values(ability: np.array, bias_difficult: np.array) -> np.array: 
+    '''
+    Calculate matrix of estimated values, step of Rasch model learning.
+        
+        Parameters:
+                ability (1d np.array): Vector of predicted subjects ability.
+                bias_difficult (1d np.array): Vector of predicted task difficult.
+                                              The average of this vector must be almost zero.
+
+        Returns: 
+                ev_matrix (2d np.array): Matrix of estimated values.
+            
+    '''   
+    ev_matrix = np.array([ability])
+    for dif_value in bias_difficult:
+        diff_exp = np.exp(ability - dif_value)
+        ev_vec = diff_exp / (1 + diff_exp)
+        ev_matrix = np.concatenate((ev_matrix, [ev_vec]), axis=0)
+    return ev_matrix[1::].T
--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from itertools import combinations_with_replacement
 import numpy as np
 import pandas as pd
 
-from irt import irt, iter_step
+from irt import prepare, predict, estimated_values
 
 
 class TestIrt(unittest.TestCase):
@@ -12,13 +12,13 @@ class TestIrt(unittest.TestCase):
     def test_irt_ones_percent(self):
         one_parts = np.linspace(0,1,11)
         for one_percent in one_parts:
-            iter_step(np.random.binomial(1, one_percent, size=(3, 4)))
+            predict(np.random.binomial(1, one_percent, size=(3, 4)))
 
     def test_irt_input_matrix_shapes(self):
         shape_borders = list(range(100))
         shapes = combinations_with_replacement(shape_borders, 2)
         for shape in shapes:
-            iter_step(np.random.binomial(1, 0.5, size=shape))
+            predict(np.random.binomial(1, 0.5, size=shape))
 
     def test_irt_pipeline(self):
             df = pd.DataFrame({
@@ -33,10 +33,20 @@ class TestIrt(unittest.TestCase):
             true_difficult = np.array([-0.69314718, 
                                        0.69314718,  
                                        -0.69314718])
-            
-            abilities, difficult = irt(df)
+            df, _, _ = prepare(df)
+            abilities, difficult = predict(df)
             self.assertTrue(np.allclose(abilities, true_abilities))
             self.assertTrue(np.allclose(difficult, true_difficult))
+
+    def test_irt_estimated_values(self):
+        true_ev_matrix = [[0.04742587, 0.01798621, 0.00669285],
+                          [0.11920292, 0.04742587, 0.01798621],
+                          [0.26894142, 0.11920292, 0.04742587]]
+        ability = np.array([1, 2, 3])        
+        diff = np.array([4, 5, 6])
+        ev_matrix = estimated_values(ability, diff)
+        self.assertTrue(np.allclose(ev_matrix, true_ev_matrix))
+
 
 if __name__ == '__main__':
     unittest.main()
