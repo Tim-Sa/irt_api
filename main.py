e6b90b657d3980906f825d4e0df7863d438c336a
Tim-Sa <salnikovtimofeyof@gmail.com>
2023-06-19 20:32:36 +0300

    update requirements.txt

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile):
-        # check the content type (MIME type)
+    # check the content type (MIME type)
     if file.content_type not in ["application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"]:
         raise HTTPException(status_code=400, detail="Invalid file type")
 
