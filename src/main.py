cc4d673f96314c51dcb0d6644d4c3fc92fd53a82
Tim-Sa <salnikovtimofeyof@gmail.com>
2024-02-24 18:46:29 +0300

    run irt calc from html form

--- src/main.py
+++ src/main.py
@@ -18,16 +18,19 @@ async def index(request: Request):
  return templates.TemplateResponse("index.html", {"request": request})
 
 
-@app.post("/upload")
+@app.post("/irt")
 async def get_logits_by_file(file: UploadFile):
 
     # check the content type (MIME type)
     if file.content_type not in avaliable_mime_types:
         raise HTTPException(status_code=400, detail="Invalid file type")
     
-    # IRT exec
-    file = await file.read()
-    df = open_xlsx(file)
+    try:
+        # IRT exec
+        file = file.file.read()
+        df = open_xlsx(file)
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=f"Can't open this file:\n\t{e}")
 
     if not df_consist_only_of(df, set([0, 1])):
         raise HTTPException(status_code=400, detail="Table must contain only one and zero values")
@@ -35,7 +38,8 @@ async def get_logits_by_file(file: UploadFile):
 
     try:
         irt_result = irt(df)
-        return vars(irt_result)
+        irt_result_dict = vars(irt_result)
+        return irt_result_dict
     except Exception as e:
         raise HTTPException(status_code=400, detail=f"Can't process this data:\n\t{e}")
     
